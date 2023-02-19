6745d499c690880126ce4f1d6dbff4a4c2c7fc81
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-02-19 13:06:08 +0100

    write log

--- amex2cvs.rb
+++ amex2cvs.rb
@@ -6,36 +6,41 @@ end
 dirname = ARGV[0]
 no_entries = 0
 
-Amex = Struct.new(:date,:value_date,:text,:amount,:cr, :lines ,keyword_init: true) do
+Amex = Struct.new(:id, :date, :value_date, :text, :amount, :cr, :lines, keyword_init: true) do
   def initialize(*args)
     super(*args)
     self.lines = []
   end
 end
-Line = Struct.new( :line, :x,:y, :part, :slice, :entry, keyword_init: true) do
+Line = Struct.new(:line, :x, :y, :part, :slice, :entry, keyword_init: true) do
   def initialize(*args)
     super(*args)
     self.part = []
+  end
+  def to_log
+    entry_id = entry ? entry.id : "+"
+    "#{x}:#{y}-#{part}/#{slice}/#{entry_id}-- #{line}"
   end
 end
 
 class Converter
   @@no_entries = 0
+
   def initialize(lines:, filename:, write_header: true)
     @current_line = 0
     @lines = parse_lines(lines)
     @log = lines.map(&:clone)
     @entries = []
-    @filename = filename    #  just for context in error/debug output
-    @data = {}  # ?
-    @write_header = write_header    # writes header once for all files
-    @slices =  Hash.new{ |hash, key| hash[key] = [] }
-    @slice_lines = Hash.new{ |hash, key| hash[key] = [] }  # ?
+    @filename = filename #  just for context in error/debug output
+    @data = {} # ?
+    @write_header = write_header # writes header once for all files
+    @slices = Hash.new { |hash, key| hash[key] = [] }
+    @slice_lines = Hash.new { |hash, key| hash[key] = [] } # ?
 
     # regexes to identify the main information parts
-    @@regex = {date: /((\d\d\.\d\d)(\d\d\.\d\d))|(CR)/,
-    amount: /([\.\d]+,\d\d)/,
-    text: /(^[A-Z][0-9 A-Z\*\.\/-]{2,}|[A-Z][0-9 A-Z\*\.\/-]{2,}$)/
+    @@regex = { date: /((\d\d\.\d\d)(\d\d\.\d\d))|(CR)/,
+                amount: /([\.\d]+,\d\d)/,
+                text: /(^[A-Z][0-9 A-Z\*\.\/-]{2,}|[A-Z][0-9 A-Z\*\.\/-]{2,}$)/
     }
     @@re_cr = /CR/
     @parts = @@regex.keys
@@ -58,8 +63,9 @@ class Converter
 
   #@@prefix_re = /(\d{4}\.\d):(\d{4}\.\d)-- /
   @@prefix_re = /^(\d{4}\.\d):(\d{4}\.\d)-- (.*)$/
+
   def parse_lines(lines)
-    lines.map do | line |
+    lines.map do |line|
       m = @@prefix_re.match(line)
       if m
         Line.new(line: m[3], x: m[1], y: m[2])
@@ -71,7 +77,7 @@ class Converter
   end
 
   def find_slices
-    @parts.each do | part |
+    @parts.each do |part|
       # puts "starting #{part}-----------"
       @lines.each_with_index do |line_struct, index|
         if @@regex[part] =~ line_struct.line
@@ -81,177 +87,179 @@ class Converter
       end
       # puts "@slices[#{part}]: #{@slices[part].inspect}"
       # group consecutive lines
-      @slices[part] = @slices[part].slice_when{|prev,cur| cur != prev + 1}.to_a
+      @slices[part] = @slices[part].slice_when { |prev, cur| cur != prev + 1 }.to_a
       # discard
-      @slices[part].reject!{|a| a.size < 4} # this might skip short last page!!!
+      @slices[part].reject! { |a| a.size < 4 } # this might skip short last page!!!
     end
-  end
 
-  # this needs to be done first to have a place to store the cr markers
-  # that need to be removed from the date slice.
-  def fill_amounts
-    # puts '---- create Entries, fill_amounts----'
-
-    @slices[:amount].each_with_index do |slice, slice_no|
-      @line_entries[slice_no] = []
-      slice.each_with_index do |index_in_line_array, index_in_slice|
-        line = @lines[index_in_line_array]
-        amount = line.line.gsub('.','').gsub(',','.').to_f
-        entry = Amex.new(amount: amount)
-        entry.lines << line
-        line.entry = entry
-        line.slice = slice_no
-        @line_entries[slice_no][index_in_slice] = entry
-        #puts @line_entries
-      end
-    end
-  end
-
-  # to find contiguous date slices, the CR markers had to be included,
-  # as they appear among the date lines in the text export.
-  # as they belong to the previous line/entry, they need to be removed and
-  # the info added to the according entry.
-  def remove_cr_lines_from_dates
-    @slices[:date].each_with_index do |slice, slice_no|
-      amount_index = 0
-      cr_indices = []
-      slice.each_with_index do |index_in_line_array, index_in_slice|
-        # puts "processing #{slice_no}/#{index_in_slice} amount_index:#{amount_index}"
-        if @lines[index_in_line_array].line =~ @@re_cr
-          cr_indices << index_in_slice
-          entry = @line_entries[slice_no][amount_index]
+    # this needs to be done first to have a place to store the cr markers
+    # that need to be removed from the date slice.
+    def fill_amounts
+      # puts '---- create Entries, fill_amounts----'
+      entry_no = 0
+      @slices[:amount].each_with_index do |slice, slice_no|
+        @line_entries[slice_no] = []
+        slice.each_with_index do |index_in_line_array, index_in_slice|
           line = @lines[index_in_line_array]
-          line.part << :cr
-          line.entry = entry
-          entry.cr = line.line
+          amount = line.line.gsub('.', '').gsub(',', '.').to_f
+          entry_no += 1
+          entry = Amex.new(id: entry_no, amount: amount)
           entry.lines << line
-          # puts "found CR in #{slice_no}/#{index_in_slice}, adding to #{entry}"
-        else
-          amount_index += 1
-        end
-      end
-      # puts 'delete:'
-      # puts slice.inspect
-      cr_indices.each { | i | slice.delete_at(i) }
-      # puts slice.inspect
-    end
-  end
-
-  def fill_dates
-    @slices[:date].each_with_index do |slice, slice_no|
-      slice.each_with_index do |index_in_line_array, index_in_slice|
-        line = @lines[index_in_line_array]
-        #    @@regex = {date: /((\d\d\.\d\d)(\d\d\.\d\d))|(CR)/,
-        m = @@regex[:date].match(line.line)
-        if m
-          entry = @line_entries[slice_no][index_in_slice]
           line.entry = entry
-          entry.lines << line
-          entry.date = m[2]
-          entry.value_date = m[3]
-          # puts
-          # puts "-------- fill_dates ----------"
-          # puts entry.inspect
-          # puts line.inspect
-          # puts
-        else
-          STDERR.puts("ERROR: found no date in fill_dates: #{line.line}")
+          line.slice = slice_no
+          @line_entries[slice_no][index_in_slice] = entry
+          # puts @line_entries
         end
       end
     end
-  end
 
-  def fill_texts
-    @slices[:text].each_with_index do |slice, slice_no|
-      slice.each_with_index do |index_in_line_array, index_in_slice|
-        line = @lines[index_in_line_array]
-        #  text: /(^[A-Z][0-9 A-Z\*\.\/-]{2,}|[A-Z][0-9 A-Z\*\.\/-]{2,}$)/
-        m = @@regex[:text].match(line.line)
-        if m
-          entry = @line_entries[slice_no][index_in_slice]
-          line.entry = entry
-          entry.lines << line
-          entry.text = m[1]
-          # puts
-          # puts "-------- fill_texts ----------"
-          # puts entry.inspect
-          # puts line.inspect
-          # puts
-        else
-          STDERR.puts("ERROR: found no text in fill_texts: #{line.line}")
+    # to find contiguous date slices, the CR markers had to be included,
+    # as they appear among the date lines in the text export.
+    # as they belong to the previous line/entry, they need to be removed and
+    # the info added to the according entry.
+    def remove_cr_lines_from_dates
+      @slices[:date].each_with_index do |slice, slice_no|
+        amount_index = 0
+        cr_indices = []
+        slice.each_with_index do |index_in_line_array, index_in_slice|
+          # puts "processing #{slice_no}/#{index_in_slice} amount_index:#{amount_index}"
+          if @lines[index_in_line_array].line =~ @@re_cr
+            cr_indices << index_in_slice
+            entry = @line_entries[slice_no][amount_index]
+            line = @lines[index_in_line_array]
+            line.part << :cr
+            line.entry = entry
+            line.slice = slice_no
+            entry.cr = line.line
+            entry.lines << line
+            # puts "found CR in #{slice_no}/#{index_in_slice}, adding to #{entry}"
+          else
+            amount_index += 1
+          end
+        end
+        # puts 'delete:'
+        # puts slice.inspect
+        cr_indices.each { |i| slice.delete_at(i) }
+        # puts slice.inspect
+      end
+    end
+
+    def fill_dates
+      @slices[:date].each_with_index do |slice, slice_no|
+        slice.each_with_index do |index_in_line_array, index_in_slice|
+          line = @lines[index_in_line_array]
+          #    @@regex = {date: /((\d\d\.\d\d)(\d\d\.\d\d))|(CR)/,
+          m = @@regex[:date].match(line.line)
+          if m
+            entry = @line_entries[slice_no][index_in_slice]
+            line.entry = entry
+            line.slice = slice_no
+            entry.lines << line
+            entry.date = m[2]
+            entry.value_date = m[3]
+            # puts
+            # puts "-------- fill_dates ----------"
+            # puts entry.inspect
+            # puts line.inspect
+            # puts
+          else
+            STDERR.puts("ERROR: found no date in fill_dates: #{line.line}")
+          end
         end
       end
     end
-  end
 
-  def check_slice_sizes
-    sizes = @parts.to_h { | part | [part, @slices[part].map { |sub_a| sub_a.size }]}
-    slice_part_sizes = []  # Hash.new{ |hash, key| hash[key] = {} }
-    # {:date=>[14, 21, 36], :amount=>[14, 21, 36], :text=>[14, 21, 36]}
-    unless sizes.values.uniq.size == 1
-      STDERR.puts "slice lengths are unequal: #{sizes}"
+    def fill_texts
+      @slices[:text].each_with_index do |slice, slice_no|
+        slice.each_with_index do |index_in_line_array, index_in_slice|
+          line = @lines[index_in_line_array]
+          #  text: /(^[A-Z][0-9 A-Z\*\.\/-]{2,}|[A-Z][0-9 A-Z\*\.\/-]{2,}$)/
+          m = @@regex[:text].match(line.line)
+          if m
+            entry = @line_entries[slice_no][index_in_slice]
+            line.entry = entry
+            line.slice = slice_no
+            entry.lines << line
+            entry.text = m[1]
+            # puts
+            # puts "-------- fill_texts ----------"
+            # puts entry.inspect
+            # puts line.inspect
+            # puts
+          else
+            STDERR.puts("ERROR: found no text in fill_texts: #{line.line}")
+          end
+        end
+      end
     end
-  end
 
-  def run_checks
-    puts "..."
-    check_slice_sizes
-  end
+    def check_slice_sizes
+      sizes = @parts.to_h { |part| [part, @slices[part].map { |sub_a| sub_a.size }] }
+      slice_part_sizes = [] # Hash.new{ |hash, key| hash[key] = {} }
+      # {:date=>[14, 21, 36], :amount=>[14, 21, 36], :text=>[14, 21, 36]}
+      unless sizes.values.uniq.size == 1
+        STDERR.puts "slice lengths are unequal: #{sizes}"
+      end
+    end
 
-  def result
-    # puts '---- inspect ----'
-    # puts @lines.join("\n")
-    # puts @slices
-    @lines.join("\n")
-  end
+    def run_checks
+      puts "..."
+      check_slice_sizes
+    end
 
-  #attr_reader :log
-  def finish_log
-    @slices.each do | part, slices |
-    slices.each_with_index do | indexes, slice_no |
-    indexes.each_with_index do | index, index_in_slice |
-    @log[index].prepend("#{part}-#{slice_no+1}-#{index_in_slice+1}--")
-  end
-  end
-  end
-  end
+    def result
+      # puts '---- inspect ----'
+      # puts @lines.join("\n")
+      # puts @slices
+      @lines.join("\n")
+    end
 
+    # attr_reader :log
+
+  end
   def log
-    finish_log
-    @log.join
+    @lines.map(&:to_log).join("\n")
   end
-
+  def timestamp
+    require 'date'
+    DateTime.now.strftime("Printed on %d.%m.%Y at %H:%M:%S")
   end
+end
 
 
-  # ---- main script
 
-  re = /\.txt/
-  globpattern = dirname =~ re ? dirname : dirname + '/*.txt'
-  files = Dir.glob(globpattern)
-  write_header = true
-  files.each do | filename |
-    begin
-      output_filename = filename.gsub(/.txt$/,'.csv')
-      log_filename = filename.gsub(/.txt$/,'.log')
 
-      lines = File.open(filename) { | f |f.readlines }
 
-      converter = Converter.new(lines: lines,filename: filename, write_header: write_header)
-      converter.parse
 
-      File.open(log_filename,'w') do | outputfile |
-        outputfile.write converter.log
-      end
-      File.open(output_filename,'w') do | outputfile |
-        outputfile.write converter.result
-      end
-      write_header = false
-    rescue Exception => e
-      puts e.message
-       puts e.backtrace.inspect
-       puts "in #{filename}"
+# ---- main script
+
+re = /\.txt/
+globpattern = dirname =~ re ? dirname : dirname + '/*.txt'
+files = Dir.glob(globpattern)
+write_header = true
+files.each do |filename|
+  begin
+    output_filename = filename.gsub(/.txt$/, '.csv')
+    log_filename = filename.gsub(/.txt$/, '.log')
+
+    lines = File.open(filename) { |f| f.readlines }
+
+    converter = Converter.new(lines: lines, filename: filename, write_header: write_header)
+    converter.parse
+    puts "writing log to #{log_filename}"
+    File.open(log_filename, 'w') do |outputfile|
+      outputfile.write converter.timestamp
+      outputfile.write converter.log
     end
+    File.open(output_filename, 'w') do |outputfile|
+      outputfile.write converter.result
+    end
+    write_header = false
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.inspect
+    puts "in #{filename}"
   end
+end
 #  puts "----------"
 #  puts "in #{filename}"
